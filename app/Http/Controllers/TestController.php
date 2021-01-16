19634e11c5e7ab15f683bb9987e42ef4b9a91c5e
imaneEmi <imane.elaimani@gmail.com>
2021-01-16 22:20:19 +0000

    home_page

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   $error="";
+        $employees=""; 
+        try {
+            //$employees = DB::table('employees')->get();
+            //DB::statement("CREATE INDEX IndexTest ON EMPLOYEES(EMPLOYEE_NAME)");
+            //DB::statement("ANALYZE INDEX IndexTest VALIDATE STRUCTURE");
+            //DB::statement('ALTER INDEX EMI.EMIINDEX1  MONITORING USAGE');
+            //DB::statement('ALTER INDEX EMI.EMIINDEX1  NOMONITORING USAGE');
+            $indexes=DB::table('dba_indexes')->where('owner','=','SYS')
+            ->join('dba_ind_columns','dba_indexes.index_name','=','dba_ind_columns.index_name')
+            ->select('dba_indexes.owner',
+            'dba_indexes.index_name',
+            'dba_indexes.table_owner',
+            'dba_indexes.table_name',
+            'dba_indexes.tablespace_name',
+            'last_analyzed')
+            ->take(10)
+            ->get();
+            return view('home', ["indexes" => $indexes]);
+        } catch (Exception $ex) {
+            $error=$ex->getMessage();
+            error_log($error);
+            return redirect('/home')->with('error', $error);
+        }
+    }
+
+    
+}
